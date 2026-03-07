5703a13d82b12915f06767fd24ef0736806f143a
As0581 <zagirovaslan1@gmail.com>
2026-03-07 19:27:15 +0400

    fixed routes(again)

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter basename="https://as0581.github.io/notes">
+        <BrowserRouter basename="/notes/">
             <App />
         </BrowserRouter>
     </StrictMode>,
